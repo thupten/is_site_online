24ac24f2e8ed36a9b1a3392092dae613911c8184
thupten <thupten1234@gmail.com>
2013-09-26 20:24:22 -0600

    fix url with http if not exist

--- website/application/modules/site/views/blocks/footer.php
+++ website/application/modules/site/views/blocks/footer.php
@@ -1,11 +1,38 @@
 	</div> <!--  end of main -->
-	<div class="container">
+	<div class="container footer">
 		<ul class="socials list-inline">
-			<li><a class="btn" href="#"><i class="icon-facebook"></i> Facebook</a></li>
-			<li><a class="btn" href="#"><i class="icon-twitter-sign"></i> Twitter</a></li>
-			<li><a class="btn" href="#"><i class="icon-google-plus"></i> Google Plus</a></li>
+			<li><div class="fb-like" data-href="http://www.veryusefulinfo.com" data-width="100" data-layout="button_count" data-show-faces="false" data-send="false"></div></li>
+			<li><a href="https://twitter.com/share" class="twitter-share-button" data-via="veryusefulinfo">Tweet</a></li>
+			<li><div class="g-plus" data-action="share" data-annotation="bubble" data-href="http://www.veryusefulinfo.com"></div></li>
 		</ul>
-		<p>&copy; iswebsitedown.com 2013</p>
+		<p class="text-success">The service on this website is 100% free. The source code for the website is open source as well. Source code hosted on <a href="https://github.com/thupten/is_site_online">GitHub</a>.</p>
+		<p>iswebsitedown.com 2013</p>
 	</div>
+	
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+
+	
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=104356919607333";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+
+<!-- Place this tag after the last share tag. -->
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+
 </body>
 </html>
--- website/application/modules/site/views/blocks/header.php
+++ website/application/modules/site/views/blocks/header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
 	<div class="container">
 		<div class="navbar-header">
-			<a class="navbar-brand" href="<?php echo site_url('site/index')?>">Mysite.com</a>
+			<a class="navbar-brand" href="<?php echo site_url('site/index')?>">veryusefulinfo.com</a>
 		</div>
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
 			<ul class="nav navbar-nav">
