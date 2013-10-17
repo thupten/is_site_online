6c5db48cc3d57b1b087b27a547c8dec5206f0b7b
Thupten <thupten1234@gmail.com>
2013-10-16 22:45:33 -0400

    fix navbar to collapse in smaller screen

--- website/application/modules/site/views/blocks/header.php
+++ website/application/modules/site/views/blocks/header.php
@@ -1,6 +1,12 @@
-<nav class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-default" role="navigation">
 	<div class="container">
 		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 			<a class="navbar-brand" href="<?php echo site_url('site/index')?>">veryusefulinfo.com</a>
 		</div>
 		<div class="collapse navbar-collapse navbar-ex1-collapse">
